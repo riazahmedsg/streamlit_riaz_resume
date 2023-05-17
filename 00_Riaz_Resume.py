e2cf4304aae7ff7d8014bca2cc5eb9f456484b31
Riaz Ahmed <39098181+riazahmedsg@users.noreply.github.com>
2023-05-17 12:20:48 -0400

    Update 00_Riaz_Resume.py

--- 00_Riaz_Resume.py
+++ 00_Riaz_Resume.py
@@ -19,7 +19,7 @@ st.set_page_config(
 st.title('Riaz Ahmed - Resume')
 st.write('#### Objective:  Remote / Hybrid opportunities in Data Science (using Python, Machine Learning)')
 
-st.title('Profile Summary')
+st.subheader('Profile Summary')
 
 
 
